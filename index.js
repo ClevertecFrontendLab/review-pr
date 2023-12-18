c86d54ffcbd2aa730d38b0fc16c95bdd382712e1
mitrofazxc <mitrofanzxc@gmail.com>
2023-12-18 18:52:02 +0300

    refactor(*): dependency update

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const github = require('@actions/github');
 const { request } = require('@octokit/request');
 const fs = require('fs');
 
+const API_URL = 'https://training.clevertec.ru';
+
 const main = async () => {
   try {
     const owner = core.getInput('owner', { required: true });
     const repo = core.getInput('repo', { required: true });
     const pull_number = core.getInput('pull_number', { required: true });
     const token = core.getInput('token', { required: true });
-    const base_url =
-      core.getInput('host', { required: false }) ||
-      'https://training.cleverland.by';
+    const base_url = core.getInput('host', { required: false }) || API_URL;
     const url = `${base_url}/pull-request/reviewed`;
 
     const octokit = new github.getOctokit(token);
@@ -42,8 +42,7 @@ const main = async () => {
     const isApproved = !Object.values(responseCommitsStatuses)
       .map(
         (item) =>
-          (item.includes('CHANGES_REQUESTED') &&
-            item[item.length - 1] === 'APPROVED') ||
+          (item.includes('CHANGES_REQUESTED') && item[item.length - 1] === 'APPROVED') ||
           (!item.includes('CHANGES_REQUESTED') && item.includes('APPROVED'))
       )
       .includes(false);
