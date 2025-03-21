a40d580dd77c48ce4520fce70a4f996a81c16aa3
Aliaksei Valadzko <61273546+ValadzkoAliaksei@users.noreply.github.com>
2025-03-21 23:37:57 +0300

    Revert "new review handling"

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const API_URL = 'https://training.clevertec.ru';
 const REQUIRED_NUMBER_OF_APPROVALS = 2;
 const CHANGES_REQUESTED_STATE = 'CHANGES_REQUESTED';
 const APPROVED_STATE = 'APPROVED';
-const PENDING_STATE = "PENDING"
 
 const main = async () => {
   try {
@@ -41,48 +40,41 @@ const main = async () => {
         return acc;
       }
 
-      acc[user.login] = acc[user.login] ?? [];
-      acc[user.login].push(state);
+      if (acc[user.login]) {
+        acc[user.login].push(state);
+      } else {
+        acc[user.login] = [state];
+      }
 
       return acc;
     }, {});
 
+    /** @type {string[][]} */
+    const reviewStatuses = Object.values(reviewUserHistory);
+    const hasEnoughReviews = reviewStatuses.length >= REQUIRED_NUMBER_OF_APPROVALS;
+    const isApproved = hasEnoughReviews && reviewStatuses.every((statusesHistory) => {
+      const reversedHistory = [...statusesHistory].reverse()
+      const approvedIndex = reversedHistory.findIndex((status) => status === APPROVED_STATE)
+      
+      if (approvedIndex === -1) {
+        return false
+      }
 
-    const reviewStatuses = Object.entries(reviewUserHistory);
-
-    /** @type {{name:string, status: string}[]} */
-    const mentorStatuses = []
-
-    reviewStatuses.forEach(([name, mentorHistory]) => {
-        let index = mentorHistory.length - 1
-
-        while(index >= 0 && (mentorHistory[index] !== APPROVED_STATE || mentorHistory[index] !== CHANGES_REQUESTED_STATE)) {
-          index--
-        }
-
-        if (index < 0) {
-         return mentorStatuses.push({ name, status: PENDING_STATE })
-        }
-
-        mentorStatuses.push({ name, status: mentorHistory[index] })
-    })
+      return !reversedHistory.slice(0, approvedIndex).includes(CHANGES_REQUESTED_STATE)
+    });
 
     const latestReview = reviewsWithoutAuthor[reviewsWithoutAuthor.length - 1]
-    const isLastChangesRequested = latestReview?.state === CHANGES_REQUESTED_STATE
-    const isLastApproved = latestReview?.state === APPROVED_STATE
+    const shouldPostReviewRequest = isApproved || latestReview?.state === CHANGES_REQUESTED_STATE
 
-    if (isLastApproved || isLastChangesRequested) {
+    if (shouldPostReviewRequest) {
       await request(`POST ${url}`, {
         data: {
           github: authorLogin,
-          link: pull_request_info.html_url,
+          isApproved,
           pullNumber: pull_number,
-          reviews: mentorStatuses
         },
       });
     }
-
-
   
   } catch (error) {
     console.log(error);
